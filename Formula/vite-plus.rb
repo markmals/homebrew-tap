f3449b15310bbd9b41eecfb531eed8743103e67f
Mark Malstrom <mark@malstrom.me>
2026-03-13 16:43:33 -0500

    Update Vite Plus formula to satisfy linter

--- Formula/vite-plus.rb
+++ Formula/vite-plus.rb
@@ -1,17 +1,15 @@
 class VitePlus < Formula
-  desc "The Unified Toolchain for the Web"
+  desc "Unified Toolchain for the Web"
   homepage "https://viteplus.dev"
   license "MIT"
-  version "0.1.11"
+  depends_on :macos
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://registry.npmjs.org/@voidzero-dev/vite-plus-cli-darwin-arm64/-/vite-plus-cli-darwin-arm64-#{version}.tgz"
-      sha256 "d47399881e58304e73d7bcd3efb8a0c67d89bce7785c5e5c405aecb69b72fd28"
-    else
-      url "https://registry.npmjs.org/@voidzero-dev/vite-plus-cli-darwin-x64/-/vite-plus-cli-darwin-x64-#{version}.tgz"
-      sha256 "aa29bf65e8b8cfb803280fa8ce03067e1ee11725b5b2a931463f83abcb541cf2"
-    end
+  if Hardware::CPU.arm?
+    url "https://registry.npmjs.org/@voidzero-dev/vite-plus-cli-darwin-arm64/-/vite-plus-cli-darwin-arm64-0.1.11.tgz"
+    sha256 "d47399881e58304e73d7bcd3efb8a0c67d89bce7785c5e5c405aecb69b72fd28"
+  else
+    url "https://registry.npmjs.org/@voidzero-dev/vite-plus-cli-darwin-x64/-/vite-plus-cli-darwin-x64-0.1.11.tgz"
+    sha256 "aa29bf65e8b8cfb803280fa8ce03067e1ee11725b5b2a931463f83abcb541cf2"
   end
 
   def install
